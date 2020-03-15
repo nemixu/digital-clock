63d681d399f019735cb9d367fca2a2e7b1dbaa93
nemixu <s.r.seagrave@gmail.com>
2020-03-15 03:16:26 +0000

    added boilerplate

---
+++ clock.js
@@ -0,0 +1,13 @@
+let day = new Date();
+let hh = day.getHours();
+let mm = day.getMinutes();
+let ss = day.getSeconds();
+
+console.log("hours", hh);
+console.log("mins", mm);
+console.log("secs", ss);
+
+
+const startClock = () => {
+    
+};
